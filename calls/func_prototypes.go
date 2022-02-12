ab4700d18cb14d9a94b9f111aaac92ebd72ed5d6
sakkis91 <ilias.sakkis@gmail.com>
2022-02-12 04:29:50 +0200

    Update func_prototypes.go

--- calls/func_prototypes.go
+++ calls/func_prototypes.go
@@ -1,4 +1,6 @@
 package calls
+//Run the below command to produce calls.go
+//mkwinsyscall.exe -output calls.go func_prototypes.go
 
 //sys SetTokenInformation(tokenHandle syscall.Token, tokenInformationClass uint32, tokenInformation uintptr, tokenInformationLength uint32) (err error) = advapi32.SetTokenInformation
 //sys ConvertStringSidToSid(stringSid *uint16, sid **structs.SID) (err error) = advapi32.ConvertStringSidToSidW
